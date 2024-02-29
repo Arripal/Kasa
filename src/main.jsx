c80d4d9df77807e37ceade18d0b20df0d784b69b
Alban P <albanpirraud@hotmail.fr>
2024-02-29 16:18:28 +0100

    Supression des fichiers inutiles, création de l'architecture de Kasa

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HomePage from './pages/HomePage/HomePage';
+import Logement from './pages/Logement/Logement';
+import About from './pages/About/About';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <HomePage />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: 'logements/:locationID',
+		element: <Logement />,
+	},
+	{
+		path: '/about',
+		element: <About />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../composants/Header/Header';
+import Footer from '../../composants/Footer/Footer';
+
+const HomePage = () => {
+	return (
+		<>
+			<Header />
+			<h1>Home Page</h1>
+			<Footer />
+		</>
+	);
+};
+
+export default HomePage;
