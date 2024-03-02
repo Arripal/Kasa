a12b189a14d0517fa00fdecdf60a6a77720af11d
Alban P <albanpirraud@hotmail.fr>
2024-03-02 18:02:09 +0100

    Création du composant Gallerie, finition du composant Card

--- src/composants/Card/Card.jsx
+++ src/composants/Card/Card.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './card.style.scss';
-import { NavLink } from 'react-router-dom';
-const Card = ({ title }) => {
+import PropTypes from 'prop-types';
+const Card = ({ title, cover }) => {
 	return (
-		<NavLink to="">
-			<article className="card">
-				<h2 className="card__title">{title}</h2>
-			</article>
-		</NavLink>
+		<article className="card" style={{ backgroundImage: `url(${cover})` }}>
+			<h2 className="card__title">{title}</h2>
+		</article>
 	);
 };
-
+Card.propTypes = {
+	title: PropTypes.string,
+	cover: PropTypes.string,
+};
+Card.defaultProps = {
+	title: 'Titre de la location',
+};
 export default Card;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../composants/Header/Header';
 import Banniere from '../../composants/Banniere/Banniere';
+import Gallerie from '../../composants/Gallerie/Gallerie';
 import Footer from '../../composants/Footer/Footer';
 import background_image from '../../assets/images/bannieres/image-desktop-accueil.webp';
 import './homepage.style.scss';
@@ -10,6 +11,7 @@ const HomePage = () => {
 			<main className="main">
 				<Header />
 				<Banniere background_image={background_image} />
+				<Gallerie />
 			</main>
 			<Footer />
 		</>
