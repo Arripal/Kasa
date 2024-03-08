1d0c366b0d2a0607677b155e9fccc732cb53074a
Alban P <albanpirraud@hotmail.fr>
2024-03-08 16:32:06 +0100

    Footer, Gallery et Card sont maintenant responsives

--- src/composants/Gallery/Gallery.jsx
+++ src/composants/Gallery/Gallery.jsx
@@ -7,9 +7,11 @@ import appartements from '../../assets/appartements/appartements.json';
 const Gallery = () => {
 	const cardsList = appartements.map((appartement) => {
 		return (
-			<NavLink to={`/logements/${appartement.id}`} key={appartement.id}>
-				<Card title={appartement.title} cover={appartement.cover} />
-			</NavLink>
+			<div className="card__container" key={appartement.id}>
+				<NavLink to={`/logements/${appartement.id}`}>
+					<Card title={appartement.title} cover={appartement.cover} />
+				</NavLink>
+			</div>
 		);
 	});
 	return <section className="gallery">{cardsList}</section>;
