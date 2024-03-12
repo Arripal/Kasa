f4d31b43afb15abf20f934b3cb440c4f890a1815
Alban P <albanpirraud@hotmail.fr>
2024-03-12 17:13:23 +0100

    Responsive du composant Error

--- src/composants/Error/Error.jsx
+++ src/composants/Error/Error.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import errorPhoto from '../../assets/images/logos/error-404.png';
 import './error.style.scss';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 const Error = () => {
 	let error__link = "Retourner à la page d'accueil";
 	return (
@@ -11,23 +10,17 @@ const Error = () => {
 			<main className="errorPage">
 				<Header />
 				<ul className="errorPage__list">
-					<li>
-						<img
-							src={errorPhoto}
-							alt="404 error"
-							className="errorPage__list-image"
-						/>
-					</li>
+					<li className="errorPage__list-image"></li>
 					<li>
 						<p className="errorPage__list-paragraph">
 							Oups! La page que
-							<br /> vous demandez n'existe pas.
+							<br className="br__mobile" /> vous demandez n'existe pas.
 						</p>
 					</li>
 					<li>
-						<NavLink to="/" className="errorPage__list-link">
+						<Link to="/" className="errorPage__list-link">
 							{error__link}
-						</NavLink>
+						</Link>
 					</li>
 				</ul>
 			</main>
