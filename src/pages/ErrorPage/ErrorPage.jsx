dc576974900b991c3e6d63b5bbacba3de96ff973
Alban P <albanpirraud@hotmail.fr>
2024-04-21 14:53:18 +0200

    Modifications de quelques styles

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
-import Error from '../../composants/Error/Error';
+import Header from '../../composants/Header/Header';
+import Footer from '../../composants/Footer/Footer';
+import './error.style.scss';
+import { Link } from 'react-router-dom';
 
-const ErrorPage = () => {
-	return <Error />;
+const Error = () => {
+	return (
+		<main className="errorPage main">
+			<section className="errorPage__content">
+				<Header />
+				<ul className="errorPage__content-list">
+					<li className="errorPage__content-list-image"></li>
+					<li>
+						<p className="errorPage__content-list-paragraph">
+							Oups! La page que
+							<br className="br__mobile" /> vous demandez n'existe pas.
+						</p>
+					</li>
+					<li>
+						<Link to="/" className="errorPage__content-list-link">
+							Retourner à la page d'accueil
+						</Link>
+					</li>
+				</ul>
+			</section>
+			<Footer />
+		</main>
+	);
 };
 
-export default ErrorPage;
+export default Error;
